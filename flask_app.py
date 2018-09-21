262f71c45ba0a092af3bd4df37434a93a0cfae28
ngoldstein23 <ngoldstein23@ufl.edu>
2018-09-20 21:13:32 -0400

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import request
+import requests
+app = Flask(__name__)
+import json
+import green_grocer
+import sys 
+
+#url of receipt image is sent via POST request 
+
+@app.route('/recieve_receipt', methods=["POST", "GET"])
+def post():
+    
+    if request.method == "POST":
+        body = request.get_json()
+        green_grocer.main(body["url"])
+        return('success')
+
+    else:
+        return('failure')    
+
+    return 'posting here'
+
+@app.route('/')
+def hello_world():
+
+    return 'Hello, World!'
+
+
